0fe897ced073405b8362d4fc1ddc49812bd81d2f
Niaz <yazzzik@list.ru>
2022-05-06 07:07:23 +0300

    add project

---
+++ src/main/java/ru/kpfu/peloton/pelotonproject/PelotonProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.kpfu.peloton.pelotonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelotonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PelotonProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kpfu/peloton/pelotonproject/PelotonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kpfu.peloton.pelotonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelotonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
